3b32507a95da6aeb624184c8e2db207b1a75b2b7
Israel Torres <IsraelTorres@users.noreply.github.com>
2017-03-29 18:29:27 -0700

    added packet loss detection
    updated 20170329 to account for total packet loss, due to ICMP admin block

--- function_tpc.sh
+++ function_tpc.sh
@@ -3,24 +3,31 @@
 # Israel Torres
 # true ping counter
 # tolerance-ping-counter
-#
+# # updated 20170329 to account for total packet loss, due to ICMP admin block
 function t_p_c () {
 #
 counter=0
+tolerance=5
 for x in {1..100}
  do
-    ping -c 1 "$domain" \
-    | grep -v -i ping \
-    | grep "timeout" \
-    | grep -v round && counter=$((counter+1)) \
-    || counter=0; echo -en "$counter:$x\t";
-
-    if [[ $"counter" -eq "5" ]]
+ 	ping -c 1 "$domain" \
+ 	| grep -v -i ping \
+ 	| grep -E 'timeout|100.0% packet loss' \
+ 	| grep -v 'round' && counter=$((counter+1)) \
+ 	|| counter=0 
+ 	
+ 	echo -en "$counter:$x\t"
+ 	
+    if [[ "$counter" -eq "$tolerance" ]]
         then
-            echo "counter equals 5; $domain \
-                has encountered bad result 5 times in a row"
+            echo "counter equals $tolerance; $domain has had bad result $tolerance times in a row"
         else
-            echo "$domain is OK"
+        	if [[ "$counter" -gt "0" ]]
+        		then
+        			echo "$domain is having issues..."
+        		else
+        			echo "$domain is OK"
+        	fi
         fi
 
     sleep 1
